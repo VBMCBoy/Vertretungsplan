8e5d85b78090c1fff8a9ea2699fc9897773353ee
Kay <VBMCBoy@users.noreply.github.com>
2015-10-08 17:56:47 +0200

    Projekt hinzugefügt
    Ich habe das Projekt aus Android Studio hinzugefügt und eine Erklärung
    in die README.md geschrieben.

---
+++ FSG-App 2/app/src/main/java/com/KayKaprolat/Praktikum/Vertretungsplan2/SettingsActivity.java
@@ -0,0 +1,117 @@
+package com.KayKaprolat.Praktikum.Vertretungsplan2;
+
+import com.KayKaprolat.Praktikum.Vertretungsplan2.R;
+
+import android.app.Activity;
+import android.app.AlertDialog;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+
+public class SettingsActivity extends Activity {
+	public static final String PREFS_NAME = "MyPrefsFile";
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.settings);
+
+		SharedPreferences settings = getSharedPreferences(PREFS_NAME, 0);
+		String BN = settings.getString("BN", "");
+		String PW = settings.getString("PW", "");
+		String Kl = settings.getString("Kl", "");
+		EditText PWtxt = (EditText) findViewById(R.id.txt_PW);
+		EditText BNtxt = (EditText) findViewById(R.id.txt_BN);
+		EditText Kltxt = (EditText) findViewById(R.id.txt_klasse);
+		Kltxt.setText(Kl);
+		BNtxt.setText(BN);
+		PWtxt.setText(PW);
+
+	}
+
+	public void onButtonClick(View view) {
+
+		if (view.getId() == R.id.btn_kontakt) {
+
+			new AlertDialog.Builder(this)
+					.setTitle("Kontakt")
+					.setMessage(
+							"Sie k�nnen mich unter kakaoh6@gmail.com erreichen."
+									+ "\n"
+									+ "Die App kann leider nur den exakten Ausruck finden, gibt es z.B. Vertretung f�r 4e wird Vertretung f�r 4de ignoriert und nicht angezeigt!")
+					.setPositiveButton(android.R.string.yes,
+							new DialogInterface.OnClickListener() {
+								public void onClick(DialogInterface dialog,
+										int which) {
+									// Tut erstmal nichts weiter
+								}
+							})
+
+					.show();
+
+		} else if (view.getId() == R.id.button1) {
+
+			EditText Klasse = (EditText) findViewById(R.id.txt_klasse);
+			String wert_klasse = Klasse.getText().toString();
+			SettingsActivity.geteilt_klasse = wert_klasse;
+			// Text aus den Textfeldern holen
+			EditText Name = (EditText) findViewById(R.id.txt_BN);
+			String wert_name = Name.getText().toString();
+
+
+			EditText Passwort = (EditText) findViewById(R.id.txt_PW);
+			String wert_PW = Passwort.getText().toString();
+
+			Intent i1 = new Intent(this, ViewerActivity.class);
+			i1.putExtra("PW", wert_PW);
+			i1.putExtra("BN", wert_name);
+			i1.putExtra("Kl", wert_klasse);
+			startActivity(i1);
+
+		} else {
+
+			EditText Klasse = (EditText) findViewById(R.id.txt_klasse);
+			String wert_klasse = Klasse.getText().toString();
+			SettingsActivity.geteilt_klasse = wert_klasse;
+			// Text aus den Textfeldern holen
+			EditText Name = (EditText) findViewById(R.id.txt_BN);
+			String wert_name = Name.getText().toString();
+
+			EditText Passwort = (EditText) findViewById(R.id.txt_PW);
+			String wert_PW = Passwort.getText().toString();
+
+			Intent i2 = new Intent(this, Viewer2Activity.class);
+			i2.putExtra("PW", wert_PW);
+			i2.putExtra("BN", wert_name);
+			i2.putExtra("Kl", wert_klasse);
+			startActivity(i2);
+		}
+
+	}
+
+	public static String geteilt_klasse = null;
+
+	@Override
+	protected void onStop() {
+		EditText sBN = (EditText) findViewById(R.id.txt_BN);
+		EditText sPW = (EditText) findViewById(R.id.txt_PW);
+		EditText sKl = (EditText) findViewById(R.id.txt_klasse);
+		String ssKl = sKl.getText().toString();
+		String ssBN = sBN.getText().toString();
+		String ssPW = sPW.getText().toString();
+		String saveKl = ssKl;
+		String saveBN = ssBN;
+		String savePW = ssPW;
+		super.onStop();
+		SharedPreferences settings = getSharedPreferences(PREFS_NAME, 0);
+		SharedPreferences.Editor editor = settings.edit();
+		editor.putString("PW", savePW);
+		editor.putString("BN", saveBN);
+		editor.putString("Kl", saveKl);
+		editor.commit();
+	}
+
+}
--- README.md
+++ README.md
@@ -1,4 +1,8 @@
-# Vertretungsplan
+﻿# Vertretungsplan
 Eine Android-App, welche den Vertretungsplan des FSG in Großröhrsdorf abruft und formatiert.
 
 Website des FSG: http://ferdinand-sauerbruch-gymnasium.de
+
+In dem Ordner *FSG-App 2* befindet sich der Quelltext, das Layout usw. Für die meisten Personen wird das nicht von Interesse sein.
+Personen, die die App haben möchten, laden bitte die *app-release.apk* herunter und installieren sie auf ein Android-Smartphone.
+Dazu muss man außerdem in den Einstellungen Fremdquellen aktivieren. Sie werden dazu beim Installationsversuch aufgefordert.
