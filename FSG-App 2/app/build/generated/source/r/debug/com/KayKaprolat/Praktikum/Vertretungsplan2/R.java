629e5ba461d94303d3bae8fc26a403d1a3c66117
VBMCBoy <VBMCBoy@users.noreply.github.com>
2018-01-24 20:20:37 +0100

    Material design, schönere Einstellungen, nach Lehrern suchen

--- FSG-App 2/app/build/generated/source/r/debug/com/KayKaprolat/Praktikum/Vertretungsplan2/R.java
+++ FSG-App 2/app/build/generated/source/r/debug/com/KayKaprolat/Praktikum/Vertretungsplan2/R.java
@@ -2132,12 +2132,9 @@ public final class R {
     public static final int abc_textfield_search_activated_mtrl_alpha=0x7f060048;
     public static final int abc_textfield_search_default_mtrl_alpha=0x7f060049;
     public static final int abc_textfield_search_material=0x7f06004a;
-    public static final int ic_info_black_24dp=0x7f06004b;
-    public static final int ic_launcher=0x7f06004c;
-    public static final int ic_notifications_black_24dp=0x7f06004d;
-    public static final int ic_settings_material=0x7f06004e;
-    public static final int ic_sync_black_24dp=0x7f06004f;
-    public static final int notification_template_icon_bg=0x7f060050;
+    public static final int ic_launcher=0x7f06004b;
+    public static final int ic_settings_material=0x7f06004c;
+    public static final int notification_template_icon_bg=0x7f06004d;
   }
   public static final class id {
     public static final int RelativeLayout1=0x7f070000;
