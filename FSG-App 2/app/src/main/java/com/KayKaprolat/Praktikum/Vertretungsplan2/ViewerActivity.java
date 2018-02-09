28aaef74754fa6eb6052ee6a576be2c93f0e4261
VBMCBoy <VBMCBoy@users.noreply.github.com>
2018-02-09 20:02:04 +0100

    Fehler gefixt, wo auch immer der herkam...

--- FSG-App 2/app/src/main/java/com/KayKaprolat/Praktikum/Vertretungsplan2/ViewerActivity.java
+++ FSG-App 2/app/src/main/java/com/KayKaprolat/Praktikum/Vertretungsplan2/ViewerActivity.java
@@ -9,19 +9,15 @@ import android.content.ContentResolver;
 import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
-import android.content.SharedPreferences.Editor;
 import android.os.Bundle;
 import android.preference.PreferenceManager;
-import android.support.v4.app.NotificationCompat.Builder;
+import android.support.v4.app.NotificationCompat;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
 import android.webkit.WebView;
 import android.widget.Toast;
-import com.KayKaprolat.Praktikum.Vertretungsplan2.R.drawable;
-import com.KayKaprolat.Praktikum.Vertretungsplan2.R.id;
-import com.KayKaprolat.Praktikum.Vertretungsplan2.R.layout;
 import java.io.BufferedInputStream;
 import java.io.ByteArrayOutputStream;
 import java.io.InputStream;
@@ -50,7 +46,7 @@ public class ViewerActivity extends Activity {
   private Account account;
 
   private static Account CreateSyncAccount(Context context) {
-    Account newAccount = new Account(ViewerActivity.ACCOUNT, ViewerActivity.ACCOUNT_TYPE);
+    Account newAccount = new Account(ACCOUNT, ACCOUNT_TYPE);
     AccountManager accountManager = (AccountManager) context.getSystemService(
         Context.ACCOUNT_SERVICE);
     if (accountManager.addAccountExplicitly(newAccount, null, null)) {
@@ -65,9 +61,9 @@ public class ViewerActivity extends Activity {
   protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
 
-    this.account = ViewerActivity.CreateSyncAccount(this.getApplicationContext());
+    account = CreateSyncAccount(getApplicationContext());
 
-    this.getActionBar().setTitle("Vertretungsplan");
+    getActionBar().setTitle("Vertretungsplan");
 
     SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(this);
 
@@ -77,42 +73,42 @@ public class ViewerActivity extends Activity {
     String wert_klasse = prefs.getString("KL", "");
     Boolean syncable = prefs.getBoolean("Benachrichtigungan", false);
 
-    this.mResolver = this.getContentResolver();
+    mResolver = getContentResolver();
 
     if (syncable) {
-      if (ContentResolver.getSyncAutomatically(this.account, ViewerActivity.AUTHORITY)) {
-        if (!(ContentResolver.isSyncPending(this.account, ViewerActivity.AUTHORITY))) {
+      if (ContentResolver.getSyncAutomatically(account, AUTHORITY)) {
+        if (!(ContentResolver.isSyncPending(account, AUTHORITY))) {
           ContentResolver
-              .addPeriodicSync(this.account, ViewerActivity.AUTHORITY, Bundle.EMPTY, 60 * 60);
+              .addPeriodicSync(account, AUTHORITY, Bundle.EMPTY, 60 * 60);
         }
       } else {
         ContentResolver.setMasterSyncAutomatically(true);
-        ContentResolver.setSyncAutomatically(this.account, ViewerActivity.AUTHORITY, true);
+        ContentResolver.setSyncAutomatically(account, AUTHORITY, true);
       }
 
     } else {
       ContentResolver.setMasterSyncAutomatically(false);
-      ContentResolver.setSyncAutomatically(this.account, ViewerActivity.AUTHORITY, false);
-      ContentResolver.cancelSync(this.account, ViewerActivity.AUTHORITY);
+      ContentResolver.setSyncAutomatically(account, AUTHORITY, false);
+      ContentResolver.cancelSync(account, AUTHORITY);
     }
 
     //prüfen ob leer
 
     if ("".equals(wert_PW) || "".equals(wert_name) || "".equals(wert_klasse)) {
       // Toast
-      Toast.makeText(this.getApplicationContext(),
+      Toast.makeText(getApplicationContext(),
           "Bitte stellen Sie Klasse / Lehrer, Benutzername und Passwort ein.", Toast.LENGTH_LONG)
           .show();
       // Einstellungen öffnen
       Intent intent = new Intent(this, SettingsActivity.class);
-      this.startActivity(intent);
+      startActivity(intent);
     } else {
 
-      this.setContentView(layout.viewer); //Layout starten
+      setContentView(R.layout.viewer); //Layout starten
       //Variablen festlegen
-      WebView webView = (WebView) this.findViewById(id.webView1);
+      WebView webView = (WebView) findViewById(R.id.webView1);
 
-      this.Laden(webView, true, wert_klasse, wert_name, wert_PW);
+      Laden(webView, true, wert_klasse, wert_name, wert_PW);
 
 
     }
@@ -123,35 +119,35 @@ public class ViewerActivity extends Activity {
   @Override
   protected void onResume() {
     super.onResume();
-    this.account = ViewerActivity.CreateSyncAccount(this);
+    account = CreateSyncAccount(this);
     SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(this);
     Boolean syncable = prefs.getBoolean("Benachrichtigungan", false);
-    this.mResolver = this.getContentResolver();
+    mResolver = getContentResolver();
 
     if (syncable) {
-      if (ContentResolver.getSyncAutomatically(this.account, ViewerActivity.AUTHORITY)) {
-        if (!(ContentResolver.isSyncPending(this.account, ViewerActivity.AUTHORITY))) {
+      if (ContentResolver.getSyncAutomatically(account, AUTHORITY)) {
+        if (!(ContentResolver.isSyncPending(account, AUTHORITY))) {
           ContentResolver
-              .addPeriodicSync(this.account, ViewerActivity.AUTHORITY, Bundle.EMPTY, 60 * 60);
+              .addPeriodicSync(account, AUTHORITY, Bundle.EMPTY, 60 * 60);
         }
       } else {
         ContentResolver.setMasterSyncAutomatically(true);
-        ContentResolver.setSyncAutomatically(this.account, ViewerActivity.AUTHORITY, true);
+        ContentResolver.setSyncAutomatically(account, AUTHORITY, true);
       }
 
     } else {
       ContentResolver.setMasterSyncAutomatically(false);
-      ContentResolver.setSyncAutomatically(this.account, ViewerActivity.AUTHORITY, false);
-      ContentResolver.cancelSync(this.account, ViewerActivity.AUTHORITY);
+      ContentResolver.setSyncAutomatically(account, AUTHORITY, false);
+      ContentResolver.cancelSync(account, AUTHORITY);
     }
 
 
   }
 
   public void notification(String title, String text) {
-    Builder mBuilder =
-        new Builder(this)
-            .setSmallIcon(drawable.ic_stat_name)
+    NotificationCompat.Builder mBuilder =
+        new NotificationCompat.Builder(this)
+            .setSmallIcon(R.drawable.ic_stat_name)
             .setContentTitle(title)
             .setContentText(text);
     Intent resultIntent = new Intent(this, ViewerActivity.class);
@@ -164,7 +160,7 @@ public class ViewerActivity extends Activity {
         );
     mBuilder.setContentIntent(resultPendingIntent);
     int ID = 1;
-    NotificationManager notificationManager = (NotificationManager) this.getSystemService(
+    NotificationManager notificationManager = (NotificationManager) getSystemService(
         Context.NOTIFICATION_SERVICE);
     notificationManager.notify(ID, mBuilder.build());
   }
@@ -179,10 +175,10 @@ public class ViewerActivity extends Activity {
     String wert_name = prefs.getString("BN", " ");
     String wert_klasse = prefs.getString("KL", " ");
 
-    this.setContentView(layout.viewer);
-    WebView webView = (WebView) this.findViewById(id.webView1);
+    setContentView(R.layout.viewer);
+    WebView webView = (WebView) findViewById(R.id.webView1);
 
-    this.Laden(webView, true, wert_klasse, wert_name, wert_PW);
+    Laden(webView, true, wert_klasse, wert_name, wert_PW);
 
   }
 
@@ -193,16 +189,16 @@ public class ViewerActivity extends Activity {
     String wert_name = prefs.getString("BN", " ");
     String wert_klasse = prefs.getString("KL", " ");
 
-    this.setContentView(layout.viewer);
-    WebView webView = (WebView) this.findViewById(id.webView1);
+    setContentView(R.layout.viewer);
+    WebView webView = (WebView) findViewById(R.id.webView1);
 
-    this.Laden(webView, false, wert_klasse, wert_name, wert_PW);
+    Laden(webView, false, wert_klasse, wert_name, wert_PW);
   }
 
 
   @Override
   public boolean onCreateOptionsMenu(Menu menu) {
-    MenuInflater inflater = this.getMenuInflater();
+    MenuInflater inflater = getMenuInflater();
     inflater.inflate(menu.main, menu);
     return true;
   }
@@ -210,7 +206,7 @@ public class ViewerActivity extends Activity {
   public void Menu_Einstellungen(MenuItem item) {
 
     Intent intent = new Intent(this, SettingsActivity.class);
-    this.startActivity(intent);
+    startActivity(intent);
 
   }
 
@@ -218,14 +214,14 @@ public class ViewerActivity extends Activity {
   public void Menu_Licenses(MenuItem item) {
 
     Intent intent = new Intent(this, LicenseActivity.class);
-    this.startActivity(intent);
+    startActivity(intent);
 
   }
 
 
   public void Menu_About(MenuItem item) {
     Intent intent = new Intent(this, AboutActivity.class);
-    this.startActivity(intent);
+    startActivity(intent);
 
   }
 
@@ -302,34 +298,34 @@ public class ViewerActivity extends Activity {
             String Plan = new String(baos.toByteArray(),
                 "windows-1252");
             // das ist grauenhaft
-            String a = ViewerActivity.this.cache(true).replaceAll(" ", "");
+            String a = cache(true).replaceAll(" ", "");
             String b = a.replaceAll("\r", "");
             String c = Plan.replaceAll("\r", "");
             String d = c.replaceAll(" ", "");
             if (!(b.equals(d))) {   // wenn der aktuelle Plan anders als der Alte ist
 
-              runOnUiThread(new Runnable() {
+              ViewerActivity.this.runOnUiThread(new Runnable() {
                 @Override
                 public void run() {
-                  Toast.makeText(ViewerActivity.this.getApplicationContext(),
+                  Toast.makeText(getApplicationContext(),
                       "Der Vertretungsplan ist neu.",
                       Toast.LENGTH_LONG).show();
                 }
               });
 
             }
-            ViewerActivity.this.speichern(Plan, true);
+            speichern(Plan, true);
 
-            ViewerActivity.this.toWebview(webView, Plan, wert_klasse, true,
+            toWebview(webView, Plan, wert_klasse, true,
                 day); // WebView, HTML, Klasse bzw Lehrer, heute?, heute Freitag?
 
 
           } catch (Exception e) {
             e.printStackTrace();
-            ViewerActivity.this.runOnUiThread(new Runnable() {
+            runOnUiThread(new Runnable() {
               @Override
               public void run() {
-                Toast.makeText(ViewerActivity.this.getApplicationContext(),
+                Toast.makeText(getApplicationContext(),
                     "Ein Fehler ist aufgetreten.",
                     Toast.LENGTH_LONG).show();
               }
@@ -416,33 +412,33 @@ public class ViewerActivity extends Activity {
             String Plan = new String(baos.toByteArray(),
                 "windows-1252");
 
-            String a = ViewerActivity.this.cache(false).replaceAll(" ", "");
+            String a = cache(false).replaceAll(" ", "");
             String b = a.replaceAll("\r", "");
             String c = Plan.replaceAll("\r", "");
             String d = c.replaceAll(" ", "");
             if (!(b.equals(d))) {   // wenn der aktuelle Plan anders als der Alte ist
 
-              runOnUiThread(new Runnable() {
+              ViewerActivity.this.runOnUiThread(new Runnable() {
                 @Override
                 public void run() {
-                  Toast.makeText(ViewerActivity.this.getApplicationContext(),
+                  Toast.makeText(getApplicationContext(),
                       "Der Vertretungsplan ist neu.",
                       Toast.LENGTH_LONG).show();
                 }
               });
 
             }
-            ViewerActivity.this.speichern(Plan, false);
+            speichern(Plan, false);
 
-            ViewerActivity.this.toWebview(webView, Plan, wert_klasse, false, day);
+            toWebview(webView, Plan, wert_klasse, false, day);
 
 
           } catch (Exception e) {
             e.printStackTrace();
-            ViewerActivity.this.runOnUiThread(new Runnable() {
+            runOnUiThread(new Runnable() {
               @Override
               public void run() {
-                Toast.makeText(ViewerActivity.this.getApplicationContext(),
+                Toast.makeText(getApplicationContext(),
                     "Ein Fehler ist aufgetreten.",
                     Toast.LENGTH_LONG).show();
               }
@@ -467,8 +463,8 @@ public class ViewerActivity extends Activity {
 
 
   private void speichern(String string, Boolean heute) {
-    SharedPreferences sharedPref = getPreferences(Context.MODE_PRIVATE);
-    Editor editor = sharedPref.edit();
+    SharedPreferences sharedPref = this.getPreferences(Context.MODE_PRIVATE);
+    SharedPreferences.Editor editor = sharedPref.edit();
     if (heute) {
       editor.putString("cache_website_heute", string);
     } else {
@@ -481,7 +477,7 @@ public class ViewerActivity extends Activity {
   }
 
   private String cache(Boolean heute) {
-    SharedPreferences sharedPref = getPreferences(Context.MODE_PRIVATE);
+    SharedPreferences sharedPref = this.getPreferences(Context.MODE_PRIVATE);
     if (heute) {
       return sharedPref.getString("cache_website_heute", "");
     } else {
@@ -492,7 +488,7 @@ public class ViewerActivity extends Activity {
 
   private void toWebview(final WebView webView, final String Plan, final String wert_klasse,
       final Boolean heute, final Integer day) {
-    this.runOnUiThread(new Runnable() {
+    runOnUiThread(new Runnable() {
       @Override
       public void run() {
 
@@ -524,8 +520,7 @@ public class ViewerActivity extends Activity {
         webView.loadData(doc2.html(), "text/html; charset=UTF-8",
             null);
 
-        ViewerActivity.this
-            .Datum_richtig(Plan, heute, day);    // macht einen Toast wenn das Datum nicht stimmt
+        Datum_richtig(Plan, heute, day);    // macht einen Toast wenn das Datum nicht stimmt
 
       }
     });
@@ -544,7 +539,7 @@ public class ViewerActivity extends Activity {
           c.add(Calendar.DATE, 1);
           date = c.getTime();
           if (!(Plan.contains(dateFormat.format(date)))) {
-            Toast.makeText(this.getApplicationContext(),
+            Toast.makeText(getApplicationContext(),
                 "Der Vertretungsplan scheint falsch zu sein.",
                 Toast.LENGTH_LONG).show();
           }
@@ -559,7 +554,7 @@ public class ViewerActivity extends Activity {
 
         case 6: // Freitag -- heute
           if (!(Plan.contains(dateFormat.format(date)))) {
-            Toast.makeText(this.getApplicationContext(),
+            Toast.makeText(getApplicationContext(),
                 "Der Vertretungsplan scheint falsch zu sein.",
                 Toast.LENGTH_LONG).show();
           }
@@ -569,13 +564,13 @@ public class ViewerActivity extends Activity {
           c.add(Calendar.DATE, 2);
           date = c.getTime();
           if (!(Plan.contains(dateFormat.format(date)))) {
-            Toast.makeText(this.getApplicationContext(),
+            Toast.makeText(getApplicationContext(),
                 "Der Vertretungsplan scheint falsch zu sein.",
                 Toast.LENGTH_LONG).show();
           }
           break;
         default: // Default
-          Toast.makeText(this.getApplicationContext(), "Ein Fehler ist aufgetreten.",
+          Toast.makeText(getApplicationContext(), "Ein Fehler ist aufgetreten.",
               Toast.LENGTH_LONG)
               .show();
           break;
@@ -595,7 +590,7 @@ public class ViewerActivity extends Activity {
           c.add(Calendar.DATE, 1);
           date = c.getTime();
           if (!(Plan.contains(dateFormat.format(date)))) {
-            Toast.makeText(this.getApplicationContext(),
+            Toast.makeText(getApplicationContext(),
                 "Der Vertretungsplan scheint falsch zu sein.",
                 Toast.LENGTH_LONG).show();
           }
@@ -605,7 +600,7 @@ public class ViewerActivity extends Activity {
           c.add(Calendar.DATE, 3);
           date = c.getTime();
           if (!(Plan.contains(dateFormat.format(date)))) {
-            Toast.makeText(this.getApplicationContext(),
+            Toast.makeText(getApplicationContext(),
                 "Der Vertretungsplan scheint falsch zu sein.",
                 Toast.LENGTH_LONG).show();
           }
@@ -615,13 +610,13 @@ public class ViewerActivity extends Activity {
           c.add(Calendar.DATE, 2);
           date = c.getTime();
           if (!(Plan.contains(dateFormat.format(date)))) {
-            Toast.makeText(this.getApplicationContext(),
+            Toast.makeText(getApplicationContext(),
                 "Der Vertretungsplan scheint falsch zu sein.",
                 Toast.LENGTH_LONG).show();
           }
           break;
         default: // Default
-          Toast.makeText(this.getApplicationContext(), "Ein Fehler ist aufgetreten.",
+          Toast.makeText(getApplicationContext(), "Ein Fehler ist aufgetreten.",
               Toast.LENGTH_LONG)
               .show();
           break;
