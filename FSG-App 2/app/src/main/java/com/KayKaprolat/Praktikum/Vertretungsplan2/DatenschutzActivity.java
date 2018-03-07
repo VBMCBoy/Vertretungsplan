afa3b85ebc8234f4ffe293366189becf45d2e21c
VBMCBoy <VBMCBoy@users.noreply.github.com>
2018-03-07 18:25:08 +0100

    Datenschutzerklärung hinzugefügt

---
+++ FSG-App 2/app/src/main/java/com/KayKaprolat/Praktikum/Vertretungsplan2/DatenschutzActivity.java
@@ -0,0 +1,14 @@
+package com.KayKaprolat.Praktikum.Vertretungsplan2;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class DatenschutzActivity extends Activity {
+
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    this.setContentView(R.layout.datenschutz_activity);
+
+  }
+}
