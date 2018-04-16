05593359e3e39cc43acb39f74a678c1a5f1cbb20
VBMCBoy <VBMCBoy@users.noreply.github.com>
2018-04-16 17:32:07 +0200

    Datenschutzerklärung gekürzt, auch in Einstellungen

--- Datenschutz.md
+++ Datenschutz.md
@@ -6,5 +6,3 @@ Firebase ist eine Google-Tochter und hat seinen Sitz in San Francisco (CA), USA.
 Die Datenschutzerklärung von Firebase finden Sie unter
 
 https://www.firebase.com/terms/privacy-policy.html .
-
-Sie können die Datenerfassung in den App-Einstellungen abschalten. Es werden keine anderen Daten durch die App erhoben.
--- FSG-App 2/app/build/generated/source/buildConfig/release/com/KayKaprolat/Praktikum/Vertretungsplan2/BuildConfig.java
+++ FSG-App 2/app/build/generated/source/buildConfig/release/com/KayKaprolat/Praktikum/Vertretungsplan2/BuildConfig.java
@@ -8,6 +8,6 @@ public final class BuildConfig {
   public static final String APPLICATION_ID = "com.KayKaprolat.Praktikum.Vertretungsplan2";
   public static final String BUILD_TYPE = "release";
   public static final String FLAVOR = "";
-  public static final int VERSION_CODE = 7;
-  public static final String VERSION_NAME = "v1.7.1";
+  public static final int VERSION_CODE = 8;
+  public static final String VERSION_NAME = "v1.8";
 }
